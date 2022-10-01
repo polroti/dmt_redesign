a00caa2aa359c41a0d89af26e128c154a3433736
Manoj Kumar <dharmakumar338@gmail.com>
2022-10-01 09:00:13 +0530

    cleanup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,11 @@ import 'package:flutter/material.dart';
 import 'screens/home.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp( const MyApp());
 }
 
 class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
-
- // const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -33,24 +31,6 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'DMT Information App',
-      theme: ThemeData(
-         brightness: Brightness.light,
-      ),
-      themeMode: currentTheme.currentTheme(),
-      darkTheme: ThemeData.dark(),
-      home: const HomePage(),
-    );
-  }
-}
-
-class MyAppss extends StatelessWidget {
-  const MyAppss({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
          brightness: Brightness.light,
       ),
